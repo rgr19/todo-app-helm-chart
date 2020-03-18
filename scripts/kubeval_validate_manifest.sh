92b4653f622ff4cb8163f7b90942ef8b7e90d9d0
rgr19 <onceawaken@outlook.com>
2020-03-18 09:42:15 +0100

    Travis AUTO commit in MODE helm

--- scripts/kubeval_validate_manifest.sh
+++ scripts/kubeval_validate_manifest.sh
@@ -5,9 +5,11 @@ set -e
 manifests=$(find manifests -name '*.yaml' | grep -v crd)
 
 for y in $manifests; do
+	# we skip on custom resource descriptions
+	# that kubeval can not handle
 	if [[ "$y" != *"crd"* ]]; then
-		echo "KUBEVAL $y -v master"
-		kubeval $y -v master
+		echo "KUBEVAL -v master $y "
+		kubeval -v master $y
 		if [ $? -ne 0 ]; then
 			exit $?
 		fi
