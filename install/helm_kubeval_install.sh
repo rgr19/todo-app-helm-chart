f9be7d9fd23ea3e079d5c3ed2cc1c99b8ce7dc2f
rgr19 <onceawaken@outlook.com>
2020-03-18 18:47:59 +0100

    Travis AUTO commit in MODE helm

--- install/helm_kubeval_install.sh
+++ install/helm_kubeval_install.sh
@@ -4,11 +4,12 @@ wget https://get.helm.sh/helm-v$HELM_VERSION-linux-amd64.tar.gz
 tar xzvf helm-v$HELM_VERSION-linux-amd64.tar.gz
 mv linux-amd64/helm helm
 chmod u+x helm
+mv helm /home/travis/bin/
 
 wget https://github.com/garethr/kubeval/releases/download/0.10.0/kubeval-linux-amd64.tar.gz
 tar xzvf kubeval-linux-amd64.tar.gz
 chmod u+x kubeval
+mv  kubeval /home/travis/bin/
 
-mv helm kubeval /home/travis/bin/
 ls -la /home/travis/bin
 /home/travis/bin/helm version
